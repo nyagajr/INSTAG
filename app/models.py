80658d451923fa6904f225c5636a3a767e6b8600
Nyagajrr <muchokijnrbenson@gmail.com>
2019-04-09 11:37:22 +0300

    creating the profile model class

--- app/models.py
+++ app/models.py
@@ -5,3 +5,11 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'images/')
+    profile_name = models.CharField(max_length =30)
+
+    # user_id = models.OneToOneField(User, on_delete=models.CASCADE,blank=True)
+    Bio = models.CharField(max_length =50)
+    def __str__(self):
+        return self.profile_name
