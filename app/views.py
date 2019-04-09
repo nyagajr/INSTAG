380ae65eeba737a2d5ea2a9c1b465c9b2cb84263
Nyagajrr <muchokijnrbenson@gmail.com>
2019-04-09 11:46:20 +0300

    importing dependencies

--- app/views.py
+++ app/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse
+import datetime as dt
+from .models import Profile,Comments,Image
+
 
 # Create your views here.
 
