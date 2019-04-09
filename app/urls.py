f18c9c5c8f406ca03026942096650d1e5c3fdbbd
Nyagajrr <muchokijnrbenson@gmail.com>
2019-04-09 11:45:25 +0300

    navigation urls

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$',views.index,name = 'index'),
+    url(r'home',views.index,name = 'index'),
+    url(r'^$',views.signup, name='sign'),
+    url(r'^profile/$',views.profile, name='profile'),
+    url(r'^search/$',views.search_results, name='search'),
+
 
 ]
