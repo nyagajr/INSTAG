1952a93c8b5b14e396fd082df092e30c526798ec
Nyagajrr <muchokijnrbenson@gmail.com>
2019-04-11 16:07:20 +0300

    tests

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.instagram= Profile(profile_photo = 'jj.jpg', profile_name ='insta', Bio ='funny')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.instagram,Profile))
+
+class CommentsTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.instagram= Comments(comment = 'wooooow')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.instagram,Comments))
+
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.tester= Image(image_name = 'NewYork', image_caption ='coolest',image = 'jj.jpg')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.tester,Image))
